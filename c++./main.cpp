6d76f649e33ca254e6a5a1bc30f54f5d84e42a19
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-08-27 17:43:19 +0800

    paixu
    1

--- c++./main.cpp
+++ c++./main.cpp
@@ -1,15 +1,28 @@
-//
-//  main.cpp
-//  c++.
-//
-//  Created by s20181102929 on 2019/8/27.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[100];
+    int i=0;
+    while((a[i]=getchar())!='='){
+        i++;
+    }
+    a[i]='\0';
+    int p=a[0]-48;
+    for(i=0;a[i]!='\0';i++){
+        if(a[i]=='+'){
+            p=p+(a[i+1]-48);
+        }
+        if(a[i]=='-'){
+            p=p-(a[i+1]-48);
+        }
+        if(a[i]=='*'){
+            p=p*(a[i+1]-48);
+        }
+        if(a[i]=='/'){
+            p=p/(a[i+1]-48);
+        }
+    }
+    printf("%d",p);
     return 0;
 }
